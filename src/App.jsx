9b6da802f5130210a69a8c41d35f587316629a6a
mortenlokkemoen <mortenlokkemoen@gmaile.com>
2022-10-26 10:56:46 +0200

    Started creating person component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Car from "./components/car/car";
+import Person from "./components/person/Person";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1> Our project starts now!</h1>
       <Car />
+      <Person />
     </div>
   );
 }
