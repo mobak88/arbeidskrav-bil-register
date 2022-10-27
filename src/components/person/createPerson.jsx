14fc94f70d64b9dcd6ed34af83eefb56510cccf6
mobak88 <mostafa0401@ymail.com>
2022-10-27 21:28:19 +0200

    Adding htmlFor to make form accessable

--- src/components/person/createPerson.jsx
+++ src/components/person/createPerson.jsx
@@ -1,66 +1,70 @@
-import React, { useState } from "react";
-import { Form, Button } from "semantic-ui-react";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import axios from 'axios';
 
 const CreatePerson = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [age, setAge] = useState(0);
   const [carsOwned, setCarsOwned] = useState(null);
 
   const sendDataToAPI = () => {
-    axios.post("http://194.32.107.29/GaAPI/person", {
+    axios.post('http://194.32.107.29/GaAPI/person', {
       firstName,
       lastName,
       age,
-      carsOwned,
+      carsOwned
     });
   };
 
   const test = () => {
-    console.log("Return to start menu");
+    console.log('Return to start menu');
   };
 
   return (
     <div>
       <Form>
         <Form.Field>
-          <label>First Name</label>
+          <label htmlFor='fname'>First Name</label>
           <input
-            name="fname"
+            name='fname'
+            id='fname'
             onChange={(e) => setFirstName(e.target.value)}
-            placeholder="First Name"
+            placeholder='First Name'
           />
         </Form.Field>
 
         <Form.Field>
-          <label>Last Name</label>
+          <label htmlFor='lname'>Last Name</label>
           <input
-            name="lname"
+            name='lname'
+            id='lname'
             onChange={(e) => setLastName(e.target.value)}
-            placeholder="Last Name"
+            placeholder='Last Name'
           />
         </Form.Field>
 
         <Form.Field>
-          <label>Age</label>
+          <label htmlFor='age'>Age</label>
           <input
-            type="number"
-            name="age"
+            type='number'
+            name='age'
+            id='age'
             onChange={(e) => setAge(e.target.value)}
-            placeholder="Your age"
+            placeholder='Your age'
           />
         </Form.Field>
 
         <Form.Field>
-          <label>Cars you own </label>
+          <label htmlFor='carOwned'>Cars you own </label>
           <input
-            name="carOwned"
+            name='carOwned'
+            id='carOwned'
             onChange={(e) => setCarsOwned(e.target.value)}
-            placeholder="Your Carbrand"
+            placeholder='Your Carbrand'
           />
         </Form.Field>
-        <Button type="submit" onClick={sendDataToAPI}>
+        <Button type='submit' onClick={sendDataToAPI}>
           Submit
         </Button>
         <Button onClick={test}>Cancel</Button>
