1ff18c1212eaf399ac6927e912a083ee6029e35a
mobak88 <mostafa0401@ymail.com>
2022-10-27 20:50:10 +0200

    Cleaning up error messages and infite loop caused bu using app component in routes inside app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
 import { useState } from "react";
 import Car from "./components/car/car";
 import CreatePerson from "./components/person/createPerson";
+import Person from "./components/person/Person";
 import useAxiosFetch from "./hooks/useAxiosFetch";
 import API_ENDPOINTS from "./api/endpoints";
 import { Routes, Route } from "react-router-dom";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
-
 
 function App() {
   const { data, fetchError, isLoading } = useAxiosFetch(`${API_ENDPOINTS.all}`);
-  if(data){
+  if (data) {
     console.log(data);
   }
 
   return (
-      <div className="main">
-       
-        <Car />
-        <Routes>
-          <Route path="/" element = { <App/> } />
-          <Route path="createUser" element = {<CreatePerson /> } />
-        </Routes>
-      </div>
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<Person />} />
+        <Route path="/createUser" element={<CreatePerson />} />
+      </Routes>
+    </div>
   );
 }
 export default App;
-
--- src/components/person/createPerson.jsx
+++ src/components/person/createPerson.jsx
@@ -1,56 +1,72 @@
-import React, { useState } from 'react';
-import { Form, Button} from 'semantic-ui-react';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import axios from "axios";
 
+const CreatePerson = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(0);
+  const [carsOwned, setCarsOwned] = useState(null);
 
-export default function CreatePerson() {
-    const [firstName , setFirstName ] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [age, setAge] = useState(0);
-    const [carsOwned, setCarsOwned] = useState(null);
+  const sendDataToAPI = () => {
+    axios.post("http://194.32.107.29/GaAPI/person", {
+      firstName,
+      lastName,
+      age,
+      carsOwned,
+    });
+  };
 
-    const sendDataToAPI = () => {
-        axios.post('http://194.32.107.29/GaAPI/person', {
-            firstName,
-            lastName,
-            age,
-            carsOwned
-        })
-    }
-    return (
-        <div>
-            <Form>
-                <Form.Field>
-                    <label>First Name</label>
-                    <input name="fname"
-                    onChange={(e) => setFirstName(e.target.value)} 
-                    placeholder='First Name' />
-                </Form.Field>
+  const test = () => {
+    console.log("Return to start menu");
+  };
 
-                <Form.Field>
-                    <label>Last Name</label>
-                    <input name="lname"
-                    onChange={(e) => setLastName(e.target.value)} 
-                    placeholder='Last Name' />
-                </Form.Field>
+  return (
+    <div>
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input
+            name="fname"
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="First Name"
+          />
+        </Form.Field>
 
-                <Form.Field>
-                    <label>Age</label>
-                    <input type = "number" name="age"
-                    onChange={(e) => setAge(e.target.value)} 
-                    placeholder='Your age' />
-                </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input
+            name="lname"
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder="Last Name"
+          />
+        </Form.Field>
 
-                <Form.Field>
-                    <label>Cars you own </label>
-                    <input name="carOwned"
-                    onChange={(e) => setCarsOwned(e.target.value)} 
-                    placeholder='Your Carbrand' />
-                </Form.Field>
-                <Button type="submit" onClick={sendDataToAPI}>Submit</Button>
-                <Button onClick ={'code to return to start menu'}>Cancel</Button>
-                </Form>
-        </div>
-    )
-}
+        <Form.Field>
+          <label>Age</label>
+          <input
+            type="number"
+            name="age"
+            onChange={(e) => setAge(e.target.value)}
+            placeholder="Your age"
+          />
+        </Form.Field>
 
+        <Form.Field>
+          <label>Cars you own </label>
+          <input
+            name="carOwned"
+            onChange={(e) => setCarsOwned(e.target.value)}
+            placeholder="Your Carbrand"
+          />
+        </Form.Field>
+        <Button type="submit" onClick={sendDataToAPI}>
+          Submit
+        </Button>
+        <Button onClick={test}>Cancel</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default CreatePerson;
