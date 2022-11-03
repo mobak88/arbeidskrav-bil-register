493847dc2c6f8692784f60b7e414bd76e5ba4703
mortenlokkemoen <mortenlokkemoen@gmaile.com>
2022-11-03 20:34:34 +0100

    Changed some styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,8 +8,8 @@ const Menu = () => {
 
     return (
        <nav className="menu-section">
-        <h1> Hey and welcome to the main page</h1>
-            <p>Here you can access your data</p>
+        <h1 className="menu-title"> Hey and welcome to the main page</h1>
+            <p className="menu-info">Here you can access your data</p>
             <div className="link-container">
                 <Link   Link to ="/person"><button className='menu-btn'>Users</button></Link>
                 <Link to ="/createUser"><button className='menu-btn'>Create new user</button></Link>
