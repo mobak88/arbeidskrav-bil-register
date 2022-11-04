eb2c8e01cf4b33fc7c8b8cb8087fc8496a8afa48
mobak88 <mostafa0401@ymail.com>
2022-11-04 12:12:40 +0100

    Working on find person

--- src/components/person/FindPerson.jsx
+++ src/components/person/FindPerson.jsx
@@ -5,57 +5,96 @@ import API_ENDPOINTS from '../../api/endpoints';
 import './Person.css';
 
 const FindPerson = () => {
-  const [isFIndingPerson, setIsFindingPerson] = useState(false);
-  const [chosenPerson, setChosenPerson] = useState('');
+  const [isFindingPerson, setIsFindingPerson] = useState(false);
+  const [chosenPerson, setChosenPerson] = useState({});
 
-  const personCtx = useContext(PersonCtx);
+  const { allPersonsData } = useContext(PersonCtx);
+
   const { data, fetchError, isLoading } = useAxiosFetch(`${API_ENDPOINTS.all}`);
-
-  useEffect(() => {
-    if (data) console.log(data);
-  }, [data]);
 
   const closeFindPerson = (e) => {
     e.preventDefault();
     setIsFindingPerson((prevState) => !prevState);
   };
 
-  console.log(chosenPerson);
+  const findPersonHandler = (id) => {
+    const newFoundPerson = allPersonsData.filter(
+      (person) => person.id === parseInt(id)
+    );
+    setChosenPerson(...newFoundPerson);
+  };
 
   return (
     <>
-      {!isFIndingPerson && (
+      {!isFindingPerson && (
         <div>
           <button onClick={() => setIsFindingPerson((prevState) => !prevState)}>
             Find Person
           </button>
         </div>
       )}
-      {isFIndingPerson && (
-        <form className='edit-person-form' action=''>
-          <div className='form-input-container'>
-            <label htmlFor='person'>Find a person:</label>
-            <select
-              name='person'
-              id='person'
-              onChange={(e) => {
-                setChosenPerson(e.target.value);
-              }}
-              defaultValue='default'
-            >
-              <option value={'default'}>Choose a person</option>
-              {data.persons &&
-                data.persons.map((person) => {
+      {isFindingPerson && (
+        <>
+          <form className='edit-person-form' action=''>
+            <div className='form-input-container'>
+              <label htmlFor='person'>Find a person:</label>
+              <select
+                name='person'
+                id='person'
+                onChange={(e) => {
+                  findPersonHandler(e.target.value);
+                }}
+                defaultValue='default'
+              >
+                <option value={'default'}>Choose a person</option>
+                {allPersonsData &&
+                  allPersonsData.map((person) => {
+                    return (
+                      <option key={person.id} value={person?.id}>
+                        {person?.firstName} {person?.lastName}
+                      </option>
+                    );
+                  })}
+              </select>
+            </div>
+            <button onClick={closeFindPerson}>Close</button>
+          </form>
+
+          <div className='person-container'>
+            <div className='information-wrapper'>
+              <h3>Information</h3>
+              <p className='first-name'>
+                Name: {chosenPerson?.firstName} {chosenPerson?.lastName}
+              </p>
+              <p className='age'>Age: {chosenPerson?.age}</p>
+            </div>
+            {!chosenPerson.carsOwned && (
+              <div>
+                <p className='cars-owned'>
+                  <b>Cars owned:</b>
+                </p>
+                <p className='cars-owned'>No cars owned</p>
+              </div>
+            )}
+            {chosenPerson.carsOwned &&
+              data.cars.map((car) => {
+                if (car.id === parseInt(chosenPerson.carsOwned)) {
                   return (
-                    <option key={person.id} value={person?.id}>
-                      {person?.firstName} {person?.lastName}
-                    </option>
+                    <div key={car.id}>
+                      <p className='cars-owned'>
+                        <b>Cars owned:</b>
+                      </p>
+                      <p className='cars-owned'>Make: {car?.make}</p>
+                      <p className='cars-owned'>Model: {car?.model}</p>
+                    </div>
                   );
-                })}
-            </select>
+                }
+              })}
+            <div className='button-wrapper'>
+              <button>Edit</button>
+            </div>
           </div>
-          <button onClick={closeFindPerson}>Close</button>
-        </form>
+        </>
       )}
     </>
   );
